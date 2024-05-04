6f59b7db1e1b1e0866935ffa316450625dbdc6ee
Marc Güell Segarra <marc@ondori.dev>
2024-05-04 12:09:17 +0200

    don't add colors when it's a low value

--- scripts/display_load.sh
+++ scripts/display_load.sh
@@ -3,24 +3,25 @@
 which=${1:-short}
 os=$(uname -s)
 case $os in
-    Linux|Darwin)
-        cpus=$( getconf _NPROCESSORS_ONLN )
-        ;;
-    FreeBSD)
-        cpus=$( getconf NPROCESSORS_ONLN )
-        ;;
-    *)
-        # What do I do now, Mum?
-        cpus=1
+Linux | Darwin)
+	cpus=$(getconf _NPROCESSORS_ONLN)
+	;;
+FreeBSD)
+	cpus=$(getconf NPROCESSORS_ONLN)
+	;;
+*)
+	# What do I do now, Mum?
+	cpus=1
+	;;
 esac
 
-
 format_load() {
-	load_info=$( awk \
-		-v "sys_load=$1" \
-		-v "cpus=$cpus" \
-		-v "half_cpus=$half_cpus" \
-		'BEGIN {
+	load_info=$(
+		awk \
+			-v "sys_load=$1" \
+			-v "cpus=$cpus" \
+			-v "half_cpus=$half_cpus" \
+			'BEGIN {
 			if ( sys_load > cpus ) {
 				print "high"
 			}
@@ -34,22 +35,19 @@ format_load() {
 	)
 
 	case $load_info in
-		high)
-			printf "#[fg=colour160]"
-			;;
-		medium)
-			printf "#[fg=colour214]"
-			;;
-		low)
-			printf "#[fg=colour70]"
-			;;
+	high)
+		printf "#[fg=colour160]"
+		;;
+	medium)
+		printf "#[fg=colour214]"
+		;;
+	low) ;;
 	esac
 	echo "$1"
 }
 
-
 declare -a uptimes
-uptimes=( $( uptime | awk -v "cpus=$cpus" '
+uptimes=($(uptime | awk -v "cpus=$cpus" '
 BEGIN {
         FS=",? "
 }
@@ -60,17 +58,18 @@ BEGIN {
 	min1=NF-2
 
 	print $min1, $min5, $min15, cpus / 2
-}' ) )
+}'))
 
 half_cpus=${uptimes[3]}
 
 case $which in
-    short)
-        format_load "${uptimes[0]}"
-        ;;
-    full)
-        echo "$(format_load "${uptimes[0]}") $(format_load "${uptimes[1]}") $(format_load "${uptimes[2]}")"
-        ;;
-    *)
-        echo "Usage: $0 [short|full]"
+short)
+	format_load "${uptimes[0]}"
+	;;
+full)
+	echo "$(format_load "${uptimes[0]}") $(format_load "${uptimes[1]}") $(format_load "${uptimes[2]}")"
+	;;
+*)
+	echo "Usage: $0 [short|full]"
+	;;
 esac
